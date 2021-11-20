a38426af857201d563d074560bc2d15fad91d8c1
sliperly <52464705+sliperly@users.noreply.github.com>
2021-11-20 14:56:52 +0400

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,6 @@
+three_digit_number = input("Введите трёхзначное число цифрами ")
+a, b, c = three_digit_number
+sum = int(a) + int(b) + int(c)
+multiplication = int(a) * int(b) * int(c)
+print(sum)
+print(multiplication)
